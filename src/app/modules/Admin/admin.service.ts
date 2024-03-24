a3f18d721081d9fbb08d685c9ec9d93e91e4cabb
jahid <jahidmorol2@gmail.com>
2024-03-24 18:07:58 +0600

    implant fiter in find admin

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -3,24 +3,29 @@ import { Prisma, PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const getAllAdmins = async (query: any) => {
+  const { searchTerm, ...filterData } = query;
   const andCondition: Prisma.AdminWhereInput[] = [];
+  const adminSearchableFields = ["name", "email"];
 
   if (query.searchTerm) {
     andCondition.push({
-      OR: [
-        {
-          name: {
-            contains: query.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearchableFields.map((field) => ({
+        [field]: {
+          contains: query.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: query.searchTerm,
-            mode: "insensitive",
-          },
+      })),
+    });
+  }
+
+  const adminFilteredFields = Object.keys(filterData);
+  if (adminFilteredFields.length > 0) {
+    andCondition.push({
+      AND: adminFilteredFields.map((key) => ({
+        [key]: {
+          equals: filterData[key],
         },
-      ],
+      })),
     });
   }
 
