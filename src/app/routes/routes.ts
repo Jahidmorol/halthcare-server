c7633755857cddb12b6372ee6e44118fd139d446
jahid <jahidmorol2@gmail.com>
2024-04-16 20:43:37 +0600

    implant specialties and others

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { UserRoutes } from "../modules/User/user.route";
 import { AdminRoutes } from "../modules/Admin/admin.route";
 import { AuthRoutes } from "../modules/Auth/auth.route";
+import { SpecialtiesRoutes } from "../modules/Specialties/specialties.route";
 
 const router = express.Router();
 
@@ -17,6 +18,10 @@ const moduleRoutes = [
   {
     path: "/admin",
     routes: AdminRoutes,
+  },
+  {
+    path: "/specialties",
+    routes: SpecialtiesRoutes,
   },
 ];
 
