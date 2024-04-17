94312608849a50044af86ee608264d426fa63ca6
jahid <jahidmorol2@gmail.com>
2024-04-17 16:51:41 +0600

    add some thing

--- src/app/modules/Doctor/doctor.route.ts
+++ src/app/modules/Doctor/doctor.route.ts
@@ -7,11 +7,27 @@ import { DoctorValidation } from "./doctor.validation";
 
 const router = express.Router();
 
+router.get("/", DoctorController.getAllFromDB);
+
+router.get("/:id", DoctorController.getByIdFromDB);
+
 router.patch(
   "/:id",
   auth(UserRole.SUPPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
   validateRequest(DoctorValidation.update),
   DoctorController.updateIntoDB
+);
+
+router.delete(
+  "/:id",
+  auth(UserRole.SUPPER_ADMIN, UserRole.ADMIN),
+  DoctorController.deleteFromDB
+);
+
+router.delete(
+  "/soft/:id",
+  auth(UserRole.SUPPER_ADMIN, UserRole.ADMIN),
+  DoctorController.softDelete
 );
 
 export const DoctorRoutes = router;
