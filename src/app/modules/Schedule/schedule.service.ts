98b8a5e68f014e687f1b0cf30fd8409478098359
jahid <jahidmorol2@gmail.com>
2024-04-22 12:30:26 +0600

    implant schedule with data

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,27 +1,44 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 
 const insertIntoDB = async (payload: any) => {
+  // console.log("schedule--=>", payload);
+
   const startDate = new Date(payload.startDate);
   const endDate = new Date(payload.endDate);
+  // const startTime = payload.startTime;
+  // const endTime = payload.endTime;
 
   while (startDate <= endDate) {
     const startDateTime = new Date(
       addHours(
         `${format(startDate, "yyyy-MM-dd")}`,
-        Number(payload.startTime.spilt(":")[0])
+        Number(payload.startTime.split(":")[0])
       )
     );
 
     const endDateTime = new Date(
       addHours(
-        `${format(endDate, "yyyy-MM-dd")}`,
-        Number(payload.endTime.spilt(":")[0])
+        `${format(startDate, "yyyy-MM-dd")}`,
+        Number(payload.endTime.split(":")[0])
       )
     );
 
-    while (startDateTime <= endDateTime) {
-      // next module.
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime,
+        endDateTime: addMinutes(startDateTime, 30),
+      };
+
+      // const result = prisma.schedule.create({
+      //   data: scheduleData,
+      // });
+
+      console.log(scheduleData);
+      startDateTime.setMinutes(startDateTime.getMinutes() + 30);
     }
+
+    startDate.setDate(startDate.getDate() + 1);
   }
 };
 
